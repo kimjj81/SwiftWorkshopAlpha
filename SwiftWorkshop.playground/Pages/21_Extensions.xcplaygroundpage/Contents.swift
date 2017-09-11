90e7c25d420ace3d8b9bef7490e7f9bf61c14505
KimJeongjin <kimjeongjin@KimJeongjinui-MacBook-Pro.local>
2017-09-11 16:53:47 +0900

    Generics 까지 완료. 2장 남았네.

---
+++ SwiftWorkshop.playground/Pages/21_Extensions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: ## Extentions
+// 이미 존재하는 클래스, 구조체, 열거형 또는 [프로토콜] 타입에 새로운 기능을 추가하는 방법이다.
+// 소스코드가 없는 것에도 적용 가능하다.(Objective-C 에서는 Extention 은 소스코드가 있을 경우이고, Cateogry 는 소스코드가 없는 경우 확장하는 것을 칭한다. Objective-C 의 Extention은 Category 의 일종이며 Extention(익명의 Category) 에는 프로퍼티 추가가 가능했다.)
+
+/*:
+ * 계산형 타입 프로퍼티와 인스턴스 프로퍼티 추가 가능
+ * 타입 메소드와 인스턴스 메소드 정의 가능
+ * 새로운 생성자 추가
+ * Subscript 선언
+ * 새로운 내포 타입을 선언하고 사용하기
+ * 기존 타입의 프로토콜 구현
+*/
+
+//: #### 안되는 것
+// Override 불가
+
+//: ##### Syntax
+// extension 
+//: [Next](@next)
